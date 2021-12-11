6ed720a5e5bce3ea03eca2829acef37ade3bf4a3
Almerindo Paixão <almerindo_paixao@live.com>
2021-12-11 14:41:35 -0300

    refactor: description

--- src/config/sinte-config.ts
+++ src/config/sinte-config.ts
@@ -3,7 +3,7 @@ export class SiteConfig {
   static SITE_TITLE =
     'Simulador de Aposentadoria | Calcule sua aposentadoria online';
   static SITE_DESCRIPTION =
-    'Acesse a nossa plataforma de simulação de aposentadoria gratuitamente e planeje quanto você deverá economizar para conquistar a sua tão sonhada aposentadoria !!';
+    'Acesse a nossa plataforma de simulação de aposentadoria gratuitamente e planeje quanto você deverá economizar para conquistar a sua tão sonhada aposentadoria.';
   static GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   static SITE_URL = 'https://simuladoraposentadoria.com.br';
 }
